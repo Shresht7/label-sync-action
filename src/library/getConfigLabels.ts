2b427c89cf566f739bb991bf8e85ddcd773adf05
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-03-13 14:06:18 +0530

    Support yaml and json configs. Fixes #13

--- src/library/getConfigLabels.ts
+++ src/library/getConfigLabels.ts
@@ -9,6 +9,7 @@ import type { GitHubLabel, LabelMap } from '../types'
 
 const workspace = process.env.GITHUB_WORKSPACE || ''
 const configPath = path.join(workspace, config.path)
+const configExt = path.extname(config.path)
 
 /** Reads labels from '.github/labels.yaml  file and returns a LabelMap */
 export async function getConfigLabels(): Promise<LabelMap> {
@@ -16,8 +17,16 @@ export async function getConfigLabels(): Promise<LabelMap> {
     //  Read config file from directory
     const file: string = await fs.promises.readFile(configPath, { encoding: 'utf-8' }).catch(_ => "")
 
+    //  Generate Config LabelMap
     const configLabels: LabelMap = new Map()
-    const labels: GitHubLabel[] = yaml.load(file) as GitHubLabel[]
+    let labels: GitHubLabel[] = []
+    if (configExt.endsWith('json')) {
+        labels = JSON.parse(file)
+    } else if (configExt.endsWith('yml') || configExt.endsWith('yaml')) {
+        labels = yaml.load(file) as GitHubLabel[]
+    } else {
+        throw new Error('Incompatible config format. Please provide a `yaml` or `json` file.')
+    }
     labels.forEach(label => configLabels.set(label.name, label))
 
     return configLabels
