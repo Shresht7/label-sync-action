00f9a1b0ca6c9ef39d60743f0ab2be7700d23e8b
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-03-13 20:20:58 +0530

    Fix bad import for config

--- src/labelSync.ts
+++ src/labelSync.ts
@@ -2,7 +2,7 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
 import { syncConfigLabels, syncRepoLabels } from './library'
-import { config } from './library'
+import * as config from './config'
 
 //  ==========
 //  LABEL SYNC
@@ -13,12 +13,12 @@ async function labelSync() {
 
     if (github.context.eventName === 'label' && !config.readonly) {     //  If the action was triggered by the label webhook event
 
-        core.notice(`Synchronizing labels from your repository to ${config.path}`)
+        core.info(`Synchronizing labels from your repository to ${config.path}`)
         await syncConfigLabels()
 
     } else {    //  If the action was triggered on push or manually by workflow dispatch
 
-        core.notice(`Synchronizing labels from ${config.path} to your repository`)
+        core.info(`Synchronizing labels from ${config.path} to your repository`)
         await syncRepoLabels()
 
     }
