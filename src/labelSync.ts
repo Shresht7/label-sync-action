97f3c441a3161fa43380e57773bd74ddb9931db8
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-03-13 13:49:20 +0530

    Use config.path for core.info messages

--- src/labelSync.ts
+++ src/labelSync.ts
@@ -2,6 +2,7 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
 import { syncConfigLabels, syncRepoLabels } from './library'
+import { config } from './library'
 
 //  ==========
 //  LABEL SYNC
@@ -12,12 +13,12 @@ async function labelSync() {
 
     if (github.context.eventName === 'label') {     //  If the action was triggered by the label webhook event
 
-        core.info('Synchronizing labels from your repository to `.github/labels.yaml`')
+        core.info(`Synchronizing labels from your repository to ${config.path}`)
         await syncConfigLabels()
 
     } else {    //  If the action was triggered on push or manually by workflow dispatch
 
-        core.info('Synchronizing labels from `.github/labels.yaml` to your repository')
+        core.info(`Synchronizing labels from ${config.path} to your repository`)
         await syncRepoLabels()
 
     }
--- src/library/index.ts
+++ src/library/index.ts
@@ -2,5 +2,6 @@
 //  LIBRARY
 //  =======
 
+export * as config from './config'
 export * from './syncRepoLabels'
 export * from './syncConfigLabels'
