28c16da8a2207bd6ea5654be3339500479ebd900
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-04-02 19:35:10 +0530

    create metadata file

--- src/config.ts
+++ src/config.ts
@@ -1,22 +1,23 @@
 //  Library
 import * as core from '@actions/core'
+import { inputs } from './metadata'
 
 //  ======
 //  CONFIG
 //  ======
 
 /** Boolean to determine if this is a dry-run */
-export const isDryRun = core.getBooleanInput('dryrun')
+export const isDryRun = core.getBooleanInput(inputs.isDryRun)
 
 /** Config file path (default: '.github/labels.yaml') */
-export const config = core.getInput('config')
+export const config = core.getInput(inputs.config)
 
 /** Permissions */
 export const permissions = {
-    create: core.getBooleanInput('create'),
-    update: core.getBooleanInput('update'),
-    delete: core.getBooleanInput('delete')
+    create: core.getBooleanInput(inputs.create),
+    update: core.getBooleanInput(inputs.update),
+    delete: core.getBooleanInput(inputs.delete)
 }
 
 /** Boolean to determine if an artifact containing an updated labels config should be created */
-export const createArtifact = core.getBooleanInput('artifact')
+export const createArtifact = core.getBooleanInput(inputs.createArtifact)
